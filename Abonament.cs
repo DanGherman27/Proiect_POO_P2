0a62046c89e82294b96e4ee1a758b0f515bf6da4
HanBeniamin <beniamin-laurentiu.han@student.upt.ro>
2026-01-30 22:50:46 +0200

    Add files via upload

---
+++ Abonament.cs
@@ -0,0 +1,15 @@
+﻿namespace Proiect_POO2;
+
+public class Abonament
+{
+    public string Tip { get; set; }
+    public DateTime DataActivare { get; set; }
+    public double Pret { get; set; }
+
+    public Abonament(string tip, LocParcare locParcare)
+    {
+        Tip = tip;
+        DataActivare = DateTime.Now;
+        Pret = locParcare.CalcularePret();
+    }
+}

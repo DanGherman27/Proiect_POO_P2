34b1ecd4c5bcfdb1eec064d353ece8e27723858a
Davinsonn <david-emanuel.galati@student.upt.ro>
2026-02-01 12:02:07 +0200

    Refactor Abonament class for JSON serialization
    Refactor Abonament class to be abstract and add JSON serialization attributes.

--- Proiect_POO_p2/Abonament.cs
+++ Proiect_POO_p2/Abonament.cs
@@ -1,9 +1,22 @@
 namespace Proiect_POO_p2;
+using System.Text.Json.Serialization;
 
-public class Abonament
+[JsonDerivedType(typeof(AbonamentAn), typeDiscriminator: "an")]
+[JsonDerivedType(typeof(AbonamentLuna), typeDiscriminator: "luna")]
+[JsonDerivedType(typeof(AbonamentZi), typeDiscriminator: "zi")]
+abstract class Abonament
 {
-    public string Tip { get; set; }
-    public DateTime DataActivare { get; set; }
-    public double Pret { get; set; }
+    public LocParcare LocParcare { get; }
+    public int Perioada { get; }
+    public DateTime DateTime { get; protected set; }
     
+    [JsonIgnore]
+    public abstract float PretFinal {get;}    
+    
+    protected Abonament(LocParcare locParcare, int perioada)
+    {
+        LocParcare = locParcare;
+        Perioada = perioada;
+        DateTime = DateTime.Now;
+    }
 }

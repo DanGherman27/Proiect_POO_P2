5238e0e6d55eb31b05db7827f250539227877d44
Davinsonn <david-emanuel.galati@student.upt.ro>
2026-02-01 12:04:10 +0200

    Refactor ManagerClienti to add new menu option

--- Proiect_POO_p2/ManagerClienti.cs
+++ Proiect_POO_p2/ManagerClienti.cs
@@ -52,23 +52,28 @@ public static class ManagerClienti
         bool client_running = true;
         while (client_running)
         {
-            Console.Clear();
+            Console.Write("\n\n");
             Console.WriteLine("1. Cumpara abonament nou");
             Console.WriteLine("2. Abonamente active (Curente)");
             Console.WriteLine("3. Istoric Abonamente (Expirate/Anulate)");
             Console.WriteLine("4. Anuleaza un abonament activ");
+            Console.WriteLine("5. Clear");
             Console.WriteLine("0. Deconectare");
             Console.Write("Optiune: ");
             int opt_client = Optiuni.Citeste();
             switch (opt_client)
             {
                 case 1:
+                    ManagerAbonamente.CumparaAbonament();
                     break;
                 case 2:
                     break;
                 case 3:
                     break;
                 case 4:
+                    break;
+                case 5:
+                    Console.Clear();
                     break;
                 
                 case 0:
@@ -79,7 +84,6 @@ public static class ManagerClienti
                     Console.WriteLine("Optiunea este invalida!");
                     break;
             }
-
         }
     }
 }

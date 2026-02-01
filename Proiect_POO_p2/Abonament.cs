acfd136f7a553311d5d69e121b78c84e771e7212
Davinsonn <david-emanuel.galati@student.upt.ro>
2026-02-01 22:35:42 +0200

    Refactor Abonament class to include IdZona property

--- Proiect_POO_p2/Abonament.cs
+++ Proiect_POO_p2/Abonament.cs
@@ -4,8 +4,9 @@ using System.Text.Json.Serialization;
 [JsonDerivedType(typeof(AbonamentAn), typeDiscriminator: "an")]
 [JsonDerivedType(typeof(AbonamentLuna), typeDiscriminator: "luna")]
 [JsonDerivedType(typeof(AbonamentZi), typeDiscriminator: "zi")]
-abstract class Abonament
+public abstract class Abonament
 {
+    public int IdZona { get; set; }
     public LocParcare LocParcare { get; }
     public int Perioada { get; }
     public DateTime DateTime { get; protected set; }
@@ -13,10 +14,18 @@ abstract class Abonament
     [JsonIgnore]
     public abstract float PretFinal {get;}    
     
-    protected Abonament(LocParcare locParcare, int perioada)
+    protected Abonament(LocParcare locParcare, int perioada, int idZona)
     {
         LocParcare = locParcare;
         Perioada = perioada;
         DateTime = DateTime.Now;
+        IdZona = idZona;
+    }
+    
+    public override string ToString()
+    {
+        return $"[{this.GetType().Name}] Zona Parcare: {IdZona}, Loc: {LocParcare.Id}, " +
+               $"Perioada: {Perioada}, Data inceperii: {DateTime:dd-MM-yyyy}, " +
+               $"Cost: {PretFinal} lei";
     }
 }

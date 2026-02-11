855f8c2beced00405670b85963e355dda7d259cd
Davinsonn <david-emanuel.galati@student.upt.ro>
2026-02-11 17:24:23 +0200

    Update AbonamentLuna.cs

--- Proiect_POO_p2/AbonamentLuna.cs
+++ Proiect_POO_p2/AbonamentLuna.cs
@@ -4,5 +4,7 @@ class AbonamentLuna : Abonament
 {
     public AbonamentLuna(LocParcare locParcare, int perioada, int idZona) : base( locParcare, perioada, idZona) { }
     
-    public override float PretFinal => (Perioada * LocParcare.CalcularePret());
+    private float Discount = 0.8f;
+    public override float PretFinal => (Perioada * LocParcare.CalcularePret() * 30 * Discount);
+
 }
